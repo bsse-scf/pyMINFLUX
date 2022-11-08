a3e91245a157f72303bec57cc661ccc2d643f934
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2022-11-08 13:49:07 +0100

    Clean up MinFluxReader class.

--- pyminflux/reader.py
+++ pyminflux/reader.py
@@ -18,84 +18,94 @@ class MinFluxReader:
         """
 
         # Store the filename
-        self.filename: Path = Path(filename)
+        self._filename: Path = Path(filename)
 
         # Initialize the data
-        self.data = None
-        self.valid_entries = None
+        self._data = None
+        self._valid_entries = None
 
-        # Is the acquisition 3D?
-        self.is_3D: bool = False
+    @property
+    def is_3d(self):
+        """Returns True is the acquisition is 3D, False otherwise."""
+        if self._data is None:
+            raise ValueError("No data loaded.")
+        return self._data["itr"].shape[1] == 10
 
-        # Keep track of last error
-        self.last_error: str = ""
+    @property
+    def loc_index(self):
+        """Return the index of the iteration from which to extract the location."""
+        return -1
+
+    @property
+    def cfr_index(self):
+        """Return the index of the iteration from which to extract the CFR."""
+        if self._data is None:
+            raise ValueError("No data loaded.")
+        return 5 if self.is_3d else 2
+
+    @property
+    def efo_index(self):
+        """Return the index of the iteration from which to extract the EFO."""
+        if self._data is None:
+            raise ValueError("No data loaded.")
+        return 8 if self.is_3d else 3
 
     @property
     def num_valid_entries(self):
         """Number of valid entries."""
-        if self.data is None:
+        if self._data is None:
             return 0
-        return self.valid_entries.sum()
+        return self._valid_entries.sum()
 
     @property
     def num_invalid_entries(self):
         """Number of valid entries."""
-        if self.data is None:
+        if self._data is None:
             return 0
-        return len(self.valid_entries) - self.valid_entries.sum()
+        return len(self._valid_entries) - self._valid_entries.sum()
 
     def load(self) -> bool:
         """Load the file."""
 
-        if not self.filename.is_file():
-            self.last_error = f"File {self.filename} does not exist."
+        if not self._filename.is_file():
+            print(f"File {self._filename} does not exist.")
             return False
 
         try:
-            self.data = np.load(str(self.filename))
+            self._data = np.load(str(self._filename))
         except ValueError as e:
-            self.last_error = f"Could not open {self.filename}: {e}"
+            print(f"Could not open {self._filename}: {e}")
             return False
 
         # Store a logical array with the valid entries
-        self.valid_entries = self.data["vld"]
+        self._valid_entries = self._data["vld"]
 
         # Return success
-        self.last_error = ""
         return True
-
-    def is_3d(self):
-        """Returns True is the acquisition is 3D, False otherwise."""
-        if self.data is None:
-            raise ValueError("No file loaded.")
-        return self.data["itr"].shape[1] == 10
 
     def process(self, valid: bool = True, scaling_factor: float = 1.0, drop_nan: bool = True) -> pd.DataFrame:
         """Returns processed dataframe for valid (or invalid) entries."""
 
         if valid:
-            indices = self.valid_entries
+            indices = self._valid_entries
         else:
-            indices = np.logical_not(self.valid_entries)
+            indices = np.logical_not(self._valid_entries)
 
         # Extract the valid time points
-        tim = self.data["tim"][indices]
+        tim = self._data["tim"][indices]
 
         # Extract the locations for the valid iterations
-        itr = self.data["itr"][indices]
-        loc_itr = itr.shape[1] - 1
-        loc = itr[:, loc_itr]["loc"]
+        itr = self._data["itr"][indices]
+        loc = itr[:, self.loc_index]["loc"]
 
         # Extract the valid identifiers
-        tid = self.data["tid"][indices]
+        tid = self._data["tid"][indices]
 
         # Extract EFO
-        efo_itr = 8 if self.is_3d() else 3
-        efo = itr[:, efo_itr]["efo"]
+        efo = itr[:, self.efo_index]["efo"]
 
         # Extract CFR
-        cfr_itr = 5 if self.is_3d() else 2
-        cfr = itr[:, cfr_itr]["cfr"]
+        cfr = itr[:, self.cfr_index]["cfr"]
 
         # Create a Pandas dataframe for the results
         df = pd.DataFrame(
@@ -117,7 +127,7 @@ class MinFluxReader:
         num_nan_values = np.sum(np.isnan(df["z"].values))
         num_non_null_values = len(df["z"].values) - num_zero_values - num_nan_values
         valid_str = "valid" if valid else "invalid"
-        num_dir_str = "3D" if self.is_3d() else "2D"
+        num_dir_str = "3D" if self.is_3d else "2D"
 
         print(f"This {valid_str} {num_dir_str} dataset has "
               f"{num_zero_values} 0.0 values and {num_nan_values} NaN values "
@@ -131,17 +141,17 @@ class MinFluxReader:
 
     def __repr__(self):
         """String representation of the object."""
-        if self.data is None:
+        if self._data is None:
             return "No file loaded."
 
         str_valid = "all valid" \
-            if len(self.data) == self.num_valid_entries else \
+            if len(self._data) == self.num_valid_entries else \
             f"{self.num_valid_entries} valid and {self.num_invalid_entries} non valid"
 
-        str_acq = "3D" if self.is_3d() else "2D"
+        str_acq = "3D" if self.is_3d else "2D"
 
-        return f"File: {self.filename.name}\n" \
-               f"Number of entries: {len(self.data)} entries ({str_valid})\n" \
+        return f"File: {self._filename.name}\n" \
+               f"Number of entries: {len(self._data)} entries ({str_valid})\n" \
                f"Acquisition: {str_acq}"
 
     def __str__(self):
