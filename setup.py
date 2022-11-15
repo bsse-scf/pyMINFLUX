7753189c1399d54d066c65e762053f1e436d3d72
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2022-11-15 11:35:23 +0100

    Remove Cython and Cython dependencies from setup.py.

--- setup.py
+++ setup.py
@@ -1,23 +1,5 @@
 from setuptools import setup, find_packages
 from distutils.util import convert_path
-from setuptools.extension import Extension
-from numpy import get_include
-from Cython.Build import cythonize
-import sysconfig
-import os
-
-
-extra_compile_args = sysconfig.get_config_var('CFLAGS')
-if extra_compile_args is not None:
-    extra_compile_args = extra_compile_args.split()
-else:
-    extra_compile_args = []
-
-if os.name == 'nt':
-    extra_compile_args += ["/O3"]
-else:
-    extra_compile_args += ["-O3"]
-
 
 main_ns = {}
 ver_path = convert_path('pyminflux/version.py')
@@ -33,16 +15,7 @@ setup(
     description='Reader, processor, and viewer of MINFLUX raw data.',
     license='TBD',
     packages=find_packages(),
-    ext_modules=cythonize([
-        Extension(
-            "pyminflux.processor.processor",
-            ["pyminflux/processor/processor.pyx"], include_dirs=[get_include()],
-            extra_compile_args=extra_compile_args
-            ),
-        ], language_level="3"
-    ),
     install_requires=[
-        'Cython',
         'jupyter',
         'matplotlib',
         'numpy',
