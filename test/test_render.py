0d5654c4ff8f48daad1eaa2a78c39c66fdd0c3e0
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2022-11-17 09:56:46 +0100

    Remove unused modules.

--- test/test_render.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-
-class TestRender(TestCase):
-    def test_render_pptk(self):
-        print("TestRender.test_render_pptk(): implement me!")
-
-    def test_render_o3d(self):
-        print("TestRender.test_render_o3d(): implement me!")
