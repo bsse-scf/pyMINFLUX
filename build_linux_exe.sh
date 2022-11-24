11d8aaf479f32d7896d71367fe4da7908afb074a
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2022-11-24 17:11:40 +0100

    Adapt script to build the python executable (continued).

--- build_linux_exe.sh
+++ build_linux_exe.sh
@@ -3,6 +3,6 @@
 # $ conda install libpython-static
 # $ conda install ccache
 # $ sudo apt install patchelf
-python -m nuitka --jobs=8 --include-module=OpenGL --noinclude-default-mode=error --follow-imports --disable-console --assume-yes-for-downloads --enable-plugin=pylint-warnings --standalone pyminflux/main.py --enable-plugin=numpy --enable-plugin=pyside6 --output-dir=./dist
+python -m nuitka pyminflux/main.py --jobs=8 --assume-yes-for-downloads --disable-console --noinclude-default-mode=error --follow-imports --standalone --include-module=OpenGL --enable-plugin=pylint-warnings --enable-plugin=numpy --enable-plugin=pyside6 --output-dir=./dist
 mv dist/main.dist/main dist/main.dist/pyminflux
-mv dist/main.dist/main dist/pyminflux
+mv dist/main.dist dist/pyminflux
