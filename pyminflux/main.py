4d2a46a5c0ff49e769d71af4aedfc1b6ee08a722
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2022-12-09 14:13:17 +0100

    Remove unused AppEventFilter class.

--- pyminflux/main.py
+++ pyminflux/main.py
@@ -5,7 +5,6 @@ from PySide6.QtWidgets import QApplication
 
 import pyminflux.resources
 
-# from ui.appeventfilter import AppEventFilter
 from pyminflux.ui.main_window import PyMinFluxMainWindow
 
 if __name__ == "__main__":
@@ -18,14 +17,5 @@ if __name__ == "__main__":
     icon = QIcon(":/icons/icon.png")
     app.setWindowIcon(icon)
     main.show()
-
-    # Attach the event filter
-    # appEventFilter = AppEventFilter()
-    # app.installEventFilter(appEventFilter)
-
-    # Add some connections
-    # appEventFilter.signal_zoom_in.connect(main.plotter.zoom_in)
-    # appEventFilter.signal_zoom_out.connect(main.plotter.zoom_out)
-    # appEventFilter.signal_delete_selection.connect(main.delete_selection)
 
     sys.exit(app.exec())
