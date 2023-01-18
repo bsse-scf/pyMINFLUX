e870e3fa4e31f573c2dfa4146d258f113839003d
Aaron Ponti <aarpon@gmail.com>
2023-01-18 11:39:18 +0100

    Expose analysis.select_by_bgmm_fitting and analysis.select_by_gmm_fitting.

--- pyminflux/analysis/__init__.py
+++ pyminflux/analysis/__init__.py
@@ -3,4 +3,6 @@ from ._analysis import (
     get_robust_threshold,
     ideal_hist_bins,
     prepare_histogram,
+    select_by_bgmm_fitting,
+    select_by_gmm_fitting,
 )
--- pyminflux/reader/_reader.py
+++ pyminflux/reader/_reader.py
@@ -350,14 +350,18 @@ class MinFluxReader:
                 self.__efo_index = 9
                 self.__cfr_index = 6
                 self.__dcr_index = 9
-                self.__eco_index = np.arange(8, 10)  # For 3D data, sum ECO values for iterations 8 and 9
+                self.__eco_index = np.arange(
+                    8, 10
+                )  # For 3D data, sum ECO values for iterations 8 and 9
                 self.__loc_index = 9
             else:
                 self.__reps = 5
                 self.__efo_index = 4
                 self.__cfr_index = 3
                 self.__dcr_index = 4
-                self.__eco_index = np.arange(4, 5)  # For 2D data, only consider ECO value at iteration 4
+                self.__eco_index = np.arange(
+                    4, 5
+                )  # For 2D data, only consider ECO value at iteration 4
                 self.__loc_index = 4
 
     def __repr__(self):
