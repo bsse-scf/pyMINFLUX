a85a58c188e6cec00c2fd2aaad810e78a8605cb6
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2023-03-13 16:17:41 +0100

    Reduce border of plotter toolbar.

--- pyminflux/ui/ui_plotter_toolbar.py
+++ pyminflux/ui/ui_plotter_toolbar.py
@@ -8,152 +8,92 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (
-    QCoreApplication,
-    QDate,
-    QDateTime,
-    QLocale,
-    QMetaObject,
-    QObject,
-    QPoint,
-    QRect,
-    QSize,
-    Qt,
-    QTime,
-    QUrl,
-)
-from PySide6.QtGui import (
-    QBrush,
-    QColor,
-    QConicalGradient,
-    QCursor,
-    QFont,
-    QFontDatabase,
-    QGradient,
-    QIcon,
-    QImage,
-    QKeySequence,
-    QLinearGradient,
-    QPainter,
-    QPalette,
-    QPixmap,
-    QRadialGradient,
-    QTransform,
-)
-from PySide6.QtWidgets import (
-    QApplication,
-    QComboBox,
-    QHBoxLayout,
-    QLabel,
-    QLayout,
-    QPushButton,
-    QSizePolicy,
-    QSpacerItem,
-    QVBoxLayout,
-    QWidget,
-)
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
+    QPushButton, QSizePolicy, QSpacerItem, QWidget)
 
 class Ui_PlotterToolbar(object):
     def setupUi(self, PlotterToolbar):
         if not PlotterToolbar.objectName():
-            PlotterToolbar.setObjectName("PlotterToolbar")
-        PlotterToolbar.resize(800, 47)
+            PlotterToolbar.setObjectName(u"PlotterToolbar")
+        PlotterToolbar.resize(791, 31)
         sizePolicy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Minimum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(PlotterToolbar.sizePolicy().hasHeightForWidth())
         PlotterToolbar.setSizePolicy(sizePolicy)
         self.horizontalLayout = QHBoxLayout(PlotterToolbar)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.main_layout = QVBoxLayout()
-        self.main_layout.setSpacing(1)
-        self.main_layout.setObjectName("main_layout")
-        self.commands_layout = QHBoxLayout()
-        self.commands_layout.setObjectName("commands_layout")
-        self.commands_layout.setSizeConstraint(QLayout.SetDefaultConstraint)
-        self.horizontalSpacer = QSpacerItem(
-            20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum
-        )
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(-1, 3, -1, 3)
+        self.horizontalSpacer = QSpacerItem(106, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.commands_layout.addItem(self.horizontalSpacer)
+        self.horizontalLayout.addItem(self.horizontalSpacer)
 
         self.lbFluorophoreIndex = QLabel(PlotterToolbar)
-        self.lbFluorophoreIndex.setObjectName("lbFluorophoreIndex")
+        self.lbFluorophoreIndex.setObjectName(u"lbFluorophoreIndex")
         self.lbFluorophoreIndex.setEnabled(True)
-        self.lbFluorophoreIndex.setAlignment(
-            Qt.AlignRight | Qt.AlignTrailing | Qt.AlignVCenter
-        )
+        self.lbFluorophoreIndex.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
 
-        self.commands_layout.addWidget(self.lbFluorophoreIndex)
+        self.horizontalLayout.addWidget(self.lbFluorophoreIndex)
 
         self.cbFluorophoreIndex = QComboBox(PlotterToolbar)
         self.cbFluorophoreIndex.addItem("")
-        self.cbFluorophoreIndex.setObjectName("cbFluorophoreIndex")
+        self.cbFluorophoreIndex.setObjectName(u"cbFluorophoreIndex")
 
-        self.commands_layout.addWidget(self.cbFluorophoreIndex)
+        self.horizontalLayout.addWidget(self.cbFluorophoreIndex)
 
         self.pbAssignFluorophores = QPushButton(PlotterToolbar)
-        self.pbAssignFluorophores.setObjectName("pbAssignFluorophores")
+        self.pbAssignFluorophores.setObjectName(u"pbAssignFluorophores")
+        self.pbAssignFluorophores.setEnabled(False)
 
-        self.commands_layout.addWidget(self.pbAssignFluorophores)
+        self.horizontalLayout.addWidget(self.pbAssignFluorophores)
 
-        self.horizontalSpacer_3 = QSpacerItem(
-            60, 20, QSizePolicy.Expanding, QSizePolicy.Minimum
-        )
+        self.horizontalSpacer_3 = QSpacerItem(105, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.commands_layout.addItem(self.horizontalSpacer_3)
+        self.horizontalLayout.addItem(self.horizontalSpacer_3)
 
         self.cbFirstParam = QComboBox(PlotterToolbar)
-        self.cbFirstParam.setObjectName("cbFirstParam")
+        self.cbFirstParam.setObjectName(u"cbFirstParam")
 
-        self.commands_layout.addWidget(self.cbFirstParam)
+        self.horizontalLayout.addWidget(self.cbFirstParam)
 
         self.cbSecondParam = QComboBox(PlotterToolbar)
-        self.cbSecondParam.setObjectName("cbSecondParam")
+        self.cbSecondParam.setObjectName(u"cbSecondParam")
 
-        self.commands_layout.addWidget(self.cbSecondParam)
+        self.horizontalLayout.addWidget(self.cbSecondParam)
 
         self.pbPlot = QPushButton(PlotterToolbar)
-        self.pbPlot.setObjectName("pbPlot")
+        self.pbPlot.setObjectName(u"pbPlot")
         sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.pbPlot.sizePolicy().hasHeightForWidth())
         self.pbPlot.setSizePolicy(sizePolicy1)
 
-        self.commands_layout.addWidget(self.pbPlot)
+        self.horizontalLayout.addWidget(self.pbPlot)
 
-        self.horizontalSpacer_2 = QSpacerItem(
-            20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum
-        )
+        self.horizontalSpacer_2 = QSpacerItem(106, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.commands_layout.addItem(self.horizontalSpacer_2)
+        self.horizontalLayout.addItem(self.horizontalSpacer_2)
 
-        self.main_layout.addLayout(self.commands_layout)
-
-        self.horizontalLayout.addLayout(self.main_layout)
 
         self.retranslateUi(PlotterToolbar)
 
         QMetaObject.connectSlotsByName(PlotterToolbar)
-
     # setupUi
 
     def retranslateUi(self, PlotterToolbar):
-        PlotterToolbar.setWindowTitle(
-            QCoreApplication.translate("PlotterToolbar", "Form", None)
-        )
-        self.lbFluorophoreIndex.setText(
-            QCoreApplication.translate("PlotterToolbar", "Fluorophore", None)
-        )
-        self.cbFluorophoreIndex.setItemText(
-            0, QCoreApplication.translate("PlotterToolbar", "1", None)
-        )
+        PlotterToolbar.setWindowTitle(QCoreApplication.translate("PlotterToolbar", u"Form", None))
+        self.lbFluorophoreIndex.setText(QCoreApplication.translate("PlotterToolbar", u"Fluorophore", None))
+        self.cbFluorophoreIndex.setItemText(0, QCoreApplication.translate("PlotterToolbar", u"1", None))
 
-        self.pbAssignFluorophores.setText(
-            QCoreApplication.translate("PlotterToolbar", "Assign", None)
-        )
-        self.pbPlot.setText(QCoreApplication.translate("PlotterToolbar", "Plot", None))
-
+        self.pbAssignFluorophores.setText(QCoreApplication.translate("PlotterToolbar", u"Assign", None))
+        self.pbPlot.setText(QCoreApplication.translate("PlotterToolbar", u"Plot", None))
     # retranslateUi
+
