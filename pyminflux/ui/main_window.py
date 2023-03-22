0720d35fcbfdf515cab4eeb3c2b98c546b3b20c6
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2023-03-22 17:35:10 +0100

    Allow opening the Analyzer from the plotter toolbar.
    # Conflicts:
    #	pyminflux/ui/main_window.py

--- pyminflux/ui/main_window.py
+++ pyminflux/ui/main_window.py
@@ -160,6 +160,7 @@ class PyMinFluxMainWindow(QMainWindow, Ui_MainWindow):
             self.plot_selected_parameters
         )
         self.plotter_toolbar.ui.pbUnmixColors.clicked.connect(self.open_color_unmixer)
+        self.plotter_toolbar.ui.pbOpenAnalyzer.clicked.connect(self.open_analyzer)
 
         # Other connections
         self.plotter.locations_selected.connect(
