5011f8ce87990e0dccbc0f1d55bd14fa93118fd1
Aaron Ponti <aarpon@gmail.com>
2023-04-17 15:15:18 +0200

    Build a macOS app with PyInstaller.

--- build_macos_app.sh
+++ build_macos_app.sh
@@ -1,2 +1,8 @@
 # To build, use the following command: currently there appears to be an issue with pyqtgraph
-pyinstaller pyminflux/main.py --noconsole --name pyminflux
+pyinstaller pyminflux/main.py \
+--hidden-import="sklearn.metrics._pairwise_distances_reduction._datasets_pair" \
+--hidden-import="sklearn.metrics._pairwise_distances_reduction._middle_term_computer" \
+--noconsole \
+--icon pyminflux/ui/assets/Logo_v3.icns \
+--name pyminflux \
+--noconfirm
