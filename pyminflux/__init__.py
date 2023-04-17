c51daa8eed30a7d5fd549878b1d9aaddc8b6a30a
Aaron Ponti <aarpon@gmail.com>
2023-04-17 16:39:57 +0200

    Change version to 0.1.0.

--- pyminflux/__init__.py
+++ pyminflux/__init__.py
@@ -1,7 +1,7 @@
 import pyqtgraph as pg
 
 __APP_NAME__ = "pyMINFLUX"
-__version__ = "0.1.0.dev0"
+__version__ = "0.1.0"
 
 # PyQtGraph settings
 pg.setConfigOption("imageAxisOrder", "row-major")  # For best performance
