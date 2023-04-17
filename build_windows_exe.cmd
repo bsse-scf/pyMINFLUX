aff7156a0dd26c0a9d198c3ef4d20833ccb98f54
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2023-04-17 17:03:59 +0200

    Fix the pyInstaller build script for Windows.

--- build_windows_exe.cmd
+++ build_windows_exe.cmd
@@ -1,9 +1,9 @@
 REM To build, use the following command
 pyinstaller.exe pyminflux\main.py ^
---windowed \
---hidden-import="sklearn.metrics._pairwise_distances_reduction._datasets_pair" \
---hidden-import="sklearn.metrics._pairwise_distances_reduction._middle_term_computer" \
+--windowed ^
+--hidden-import="sklearn.metrics._pairwise_distances_reduction._datasets_pair" ^
+--hidden-import="sklearn.metrics._pairwise_distances_reduction._middle_term_computer" ^
 --noconsole ^
---icon pyminflux/ui/assets/Logo_v3.ico \
---name pyMINFLUX \
+--icon pyminflux/ui/assets/Logo_v3.ico ^
+--name pyMINFLUX ^
 --noconfirm
