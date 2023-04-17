936d127c490f8f3e2f5858e5adc455c51ae59c0a
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2023-04-17 16:49:31 +0200

    Add macOS x86_64 and Windows pyInstaller build scripts.

--- build_linux_exe.sh
+++ build_linux_exe.sh
@@ -1,2 +1,8 @@
 # To build, use the following command
-pyinstaller pyminflux/main.py --noconsole --name pyminflux
+pyinstaller pyminflux/main.py \
+--clean \
+--windowed \
+--icon pyminflux/ui/assets/Logo_v3.png \
+--noconsole \
+--name pyMINFLUX \
+--noconfirm
--- build_macos_app_x86_64.sh
+++ build_macos_app_x86_64.sh
@@ -1,9 +1,10 @@
-REM To build, use the following command
-pyinstaller.exe pyminflux\main.py ^
+# To build, use the following command: currently there appears to be an issue with pyqtgraph
+pyinstaller pyminflux/main.py \
 --windowed \
 --hidden-import="sklearn.metrics._pairwise_distances_reduction._datasets_pair" \
 --hidden-import="sklearn.metrics._pairwise_distances_reduction._middle_term_computer" \
---noconsole ^
---icon pyminflux/ui/assets/Logo_v3.ico \
+--noconsole \
+--icon pyminflux/ui/assets/Logo_v3.icns \
 --name pyMINFLUX \
+--target-architecture=x86_64 \
 --noconfirm
