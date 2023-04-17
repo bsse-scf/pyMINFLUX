5a17d5e91fee9bc13caf1b9d884e7810dcfa4328
Aaron Ponti <aarpon@gmail.com>
2023-04-17 15:41:07 +0200

    Upgrade pyInstaller.

--- build_macos_app.sh
+++ build_macos_app.sh
@@ -4,5 +4,5 @@ pyinstaller pyminflux/main.py \
 --hidden-import="sklearn.metrics._pairwise_distances_reduction._middle_term_computer" \
 --noconsole \
 --icon pyminflux/ui/assets/Logo_v3.icns \
---name pyminflux \
+--name pyMINFLUX \
 --noconfirm
