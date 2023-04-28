4fa390f7ca3acc8755953ab55df7393ef0549af3
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2023-04-28 14:28:48 +0200

    Clean build and packaging script for macOS (x86_64 and arm64).

--- build_macos_app_x86_64.sh
+++ build_macos_app_x86_64.sh
@@ -1,4 +1,3 @@
-#
 # Copyright (c) 2022 - 2023 D-BSSE, ETH Zurich.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,9 +12,29 @@
 # See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-#
 
-# To build, use the following command: currently there appears to be an issue with pyqtgraph
+VERSION=0.1.0
+
+if [[ -z "$ANACONDA_HOME" ]]; then
+    echo "Please set environment variable to ANACONDA_HOME." 1>&2
+    exit 1
+fi
+
+# Source conda.sh
+source $ANACONDA_HOME/etc/profile.d/conda.sh
+
+# Create and activate a dedicated env
+conda create -n pyminflux-build python=3.11 -y
+conda activate pyminflux-build
+
+# Install dependencies
+poetry install
+
+# Delete build and dist folders
+rm -fR build
+rm -fR dist
+
+# Build the app
 pyinstaller pyminflux/main.py \
 --windowed \
 --hidden-import="sklearn.metrics._pairwise_distances_reduction._datasets_pair" \
@@ -27,3 +46,15 @@ pyinstaller pyminflux/main.py \
 --osx-bundle-identifier 'ch.ethz.pyminflux' \
 --noconfirm
 
+
+# Copy the icon
+cp pyminflux/ui/assets/Logo_v3.png dist/pyMINFLUX
+
+# Zip the archive
+cd dist
+zip -r pyMINFLUX_${VERSION}_macos.zip pyMINFLUX.app
+cd ..
+
+# Remove the conda environment
+conda deactivate
+conda env remove -n pyminflux-build
