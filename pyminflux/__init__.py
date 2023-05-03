fe4d68acc5cf0e5fa0786fbfa4439955f371564b
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2023-05-03 09:33:00 +0200

    Increase version to 0.1.1.

--- pyminflux/__init__.py
+++ pyminflux/__init__.py
@@ -16,7 +16,7 @@
 import pyqtgraph as pg
 
 __APP_NAME__ = "pyMINFLUX"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # PyQtGraph settings
 pg.setConfigOption("imageAxisOrder", "row-major")  # For best performance
