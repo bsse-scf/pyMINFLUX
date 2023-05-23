b5d01e582a48c16f198f8871ad959e1e8b637e23
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2023-05-23 15:30:02 +0200

    Reduce usage of term "Color" and use "Fluorophore" instead.

--- pyminflux/ui/ui_wizard.py
+++ pyminflux/ui/ui_wizard.py
@@ -1,20 +1,5 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2022 - 2023 D-BSSE, ETH Zurich.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#   limitations under the License.
-#
-
 ################################################################################
 ## Form generated from reading UI file 'wizard.ui'
 ##
@@ -309,13 +294,13 @@ class Ui_WizardDialog(object):
             QCoreApplication.translate("WizardDialog", "DCR unmixing", None)
         )
         self.pbSingleColor.setText(
-            QCoreApplication.translate("WizardDialog", "Single Color", None)
+            QCoreApplication.translate("WizardDialog", "Single fluorophore", None)
         )
         self.pbColorUnmixer.setText(
             QCoreApplication.translate("WizardDialog", "Unmixer", None)
         )
         self.lbActiveColor.setText(
-            QCoreApplication.translate("WizardDialog", "Active color", None)
+            QCoreApplication.translate("WizardDialog", "Active fluorophore", None)
         )
         self.cmActiveColor.setItemText(
             0, QCoreApplication.translate("WizardDialog", "All", None)
