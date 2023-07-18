9359eb3d4f5d70b28119ffeb1968b12f0b697fd3
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2023-07-18 12:30:43 +0200

    Add target directory for build_docs.sh.

--- build_docs.sh
+++ build_docs.sh
@@ -1,2 +1,23 @@
-rm -fR doc
-pdoc3 pyminflux --html --force --output-dir doc --template-dir templates
+#!/bin/bash
+
+if [ "$#" != "1" ]; then
+    echo "Usage: ./build_docs.sh /path/to/docs/folder"
+    exit
+fi
+
+if [ -d "$1" ]
+then
+    dir_name=$(basename "$1")
+    parent_path=$(dirname "$1")
+    current_date=$(date +%Y%m%d_%H%M%S)
+    mv "$1" "$parent_path/${dir_name}_$current_date"
+    echo "$1 renamed to $parent_path/${dir_name}_$current_date"
+fi
+
+# Build documentation
+pdoc3 pyminflux --html --force --output-dir $1 --template-dir templates
+
+# Wrong references are added to the code -- unclear why
+find . -name *.html -exec sed -i 's/pyminflux.reader._reader.MinFluxReader/pyminflux.reader.MinFluxReader/g' {} \;
+find . -name *.html -exec sed -i 's/pyminflux.processor._processor.MinFluxProcessor/pyminflux.processor.MinFluxProcessor/g' {} \;
+
