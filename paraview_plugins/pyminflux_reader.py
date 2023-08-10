54054c5f18c2a2edd6639eecccbef4a94a6ba65d
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2023-08-10 16:35:27 +0200

    Show localization and data in two views of a dedicated pyMINFLUX layout.

--- paraview_plugins/pyminflux_reader.py
+++ paraview_plugins/pyminflux_reader.py
@@ -1,15 +1,23 @@
 import pandas as pd
 from paraview.simple import (
+    CreateLayout,
+    CreateView,
+    Delete,
     FindSource,
     GetActiveViewOrCreate,
     GetColorTransferFunction,
+    GetLayouts,
+    GetOpacityTransferFunction,
     Hide,
+    RemoveLayout,
     Render,
     ResetCamera,
+    SetActiveView,
     Show,
     TableToPoints,
     TrivialProducer,
     UpdatePipeline,
+    _DisableFirstRenderCameraReset,
     servermanager,
 )
 from paraview.util.vtkAlgorithm import (
@@ -50,6 +58,9 @@ class pyMINFLUXReader(VTKPythonAlgorithmBase):
 
     def RequestData(self, request, inInfo, outInfo):
 
+        # Create a new "pyMINFLUX" layout
+        layout, render_view, spreadsheet_view = self.create_new_layout()
+
         # Read the file into a table compatible with TableToPoints
         table = self.file_to_table(self._filename)
 
@@ -59,46 +70,22 @@ class pyMINFLUXReader(VTKPythonAlgorithmBase):
         table_to_points.YColumn = self._y_column
         table_to_points.ZColumn = self._z_column
 
-        # Get the active render view
-        render_view = GetActiveViewOrCreate("RenderView")
-
         # Show the points in the render view
         point_display = Show(table_to_points, render_view)
+        Render(render_view)
 
-        # Change the representation to 'Points'
-        point_display.Representation = "Points"
-
-        # Change the point size
-        point_display.PointSize = 5.0
-
-        # Render points as spheres
-        point_display.RenderPointsAsSpheres = 1
-
-        # Set the Coloring based on a specific attribute
-        # Replace 'attribute_name' with the name of the attribute you want to use
-        attribute_name = "tid"
-        point_display.ColorArrayName = ("POINTS", attribute_name)
-
-        # Create the lookup table
-        lookup_table = GetColorTransferFunction(attribute_name)
-
-        # Fetch the VTK dataset from the source
-        vtk_data = servermanager.Fetch(table_to_points)
-
-        # Get the range of the selected attribute
-        data_range = vtk_data.GetPointData().GetArray(attribute_name).GetRange()
-
-        # Rescale the lookup table to match the data range
-        lookup_table.RescaleTransferFunction(data_range[0], data_range[1])
-
-        # Assign the lookup table to the display properties
-        point_display.LookupTable = lookup_table
+        # Set the display properties of the render view
+        self.set_render_view_display_properties(point_display, table_to_points)
 
         # Update the pipeline
         UpdatePipeline(proxy=point_display)
 
-        # Render the view
-        Render(render_view)
+        # Show the table object in the spreadsheet view
+        Show(table, spreadsheet_view)
+        Render(spreadsheet_view)
+
+        # Focus on the render view
+        SetActiveView(render_view)
 
         # Reset the camera
         ResetCamera()
@@ -134,3 +121,57 @@ class pyMINFLUXReader(VTKPythonAlgorithmBase):
         producer.GetClientSideObject().SetOutput(table)
 
         return producer
+
+    def create_new_layout(self):
+        """Create a new layout with a render and a spreadsheet views."""
+
+        # Create a new layout
+        layout = CreateLayout("pyMINFLUX")
+        layout.SplitHorizontal(0, 0.70)
+
+        # Create a render view
+        render_view = CreateView("RenderView", "Localizations")
+        render_view.AxesGrid = "GridAxes3DActor"
+
+        # Create a new spreadsheet view
+        spreadsheet_view = CreateView("SpreadSheetView", "Data Viewer")
+        spreadsheet_view.FieldAssociation = "Row Data"
+
+        # Assign the views to the new layout
+        layout.AssignView(1, render_view)
+        layout.AssignView(2, spreadsheet_view)
+
+        # Return the created objects
+        return layout, render_view, spreadsheet_view
+
+    def set_render_view_display_properties(self, display, table_to_points):
+        """Sets the display properties of passed view."""
+
+        # Change the representation to 'Points'
+        display.Representation = "Points"
+
+        # Change the point size
+        display.PointSize = 5.0
+
+        # Render points as spheres
+        display.RenderPointsAsSpheres = 1
+
+        # Set the Coloring based on a specific attribute
+        # Replace 'attribute_name' with the name of the attribute you want to use
+        attribute_name = "tid"
+        display.ColorArrayName = ("POINTS", attribute_name)
+
+        # Create the lookup table
+        lookup_table = GetColorTransferFunction(attribute_name)
+
+        # Fetch the VTK dataset from the source
+        vtk_data = servermanager.Fetch(table_to_points)
+
+        # Get the range of the selected attribute
+        data_range = vtk_data.GetPointData().GetArray(attribute_name).GetRange()
+
+        # Rescale the lookup table to match the data range
+        lookup_table.RescaleTransferFunction(data_range[0], data_range[1])
+
+        # Assign the lookup table to the display properties
+        display.LookupTable = lookup_table
